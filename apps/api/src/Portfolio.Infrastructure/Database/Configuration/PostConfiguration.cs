180ab977738118a6b7b3b1b7cfdf4d622c79777d
Alvin Jorrel Pascual <me@ajx.codes>
2026-06-03 12:27:17 -0600

    chore(devops): fix dockerfiles and API deps

--- apps/api/src/Portfolio.Infrastructure/Database/Configuration/PostConfiguration.cs
+++ apps/api/src/Portfolio.Infrastructure/Database/Configuration/PostConfiguration.cs
@@ -1,7 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using Portfolio.Domain.Blog;
-using Portfolio.Faker.Blog;
 
 namespace Portfolio.Infrastructure.Database.Configuration;
 
